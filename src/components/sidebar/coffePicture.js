e899c7f35f7bbff08b7b927ce9517c0316f90ebf
schreiandrej <schreiandrej@outlook.de>
2021-07-10 17:45:57 +0200

    added a search-field for the coffe pic

--- src/components/sidebar/coffePicture.js
+++ src/components/sidebar/coffePicture.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { createApi } from 'unsplash-js'
-
+import { useForm } from 'react-hook-form'
 const unsplashApi = createApi({
   accessKey: '65umxT49424gR-yCnG-e7GDQkE8DkJyrnt0VyorxUs4',
 })
@@ -8,6 +8,7 @@ const unsplashApi = createApi({
 const PhotoComp = ({ photo, setChangeImage }) => {
   const { user, urls } = photo
   const imageRef = useRef(null)
+
 
   return (
     <div className='relative'>
@@ -31,11 +32,24 @@ const PhotoComp = ({ photo, setChangeImage }) => {
 export const CoffePicOfTheDay = () => {
   const [data, setPhotosResponse] = useState(null)
   const [changeImage, setChangeImage] = useState(false)
+  const { register, handleSubmit, errors } = useForm()
+  const [searchWord, setSearchWord] = useState('coffee')
+
+  const onSubmit = (data) => {
+    console.log(data.searchWord.length)
+    
+    if (data.searchWord.length > 0) { 
+      console.log('i am here');
+      
+      setSearchWord(data.searchWord)
+      setChangeImage(!changeImage)
+  }
+  }
 
   useEffect(() => {
     unsplashApi.search
       .getPhotos({
-        query: 'good morning',
+        query: `${searchWord}`,
         orientation: 'landscape',
         page: Math.floor(Math.random() * 100),
         per_page: 1,
@@ -59,12 +73,27 @@ export const CoffePicOfTheDay = () => {
     )
   } else {
     return (
-      <>
+      <div className='flex flex-col gap-2'>
+        <form
+          className='flex flex-row w-full gap-2 max-h-10'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            type='text'
+            name='searchWord'
+            placeholder='search'
+            className='w-2/3'
+            ref={register}
+          />
+          <button type='submit' className='button-outlined w-1/3 m-0'>
+            search
+          </button>
+        </form>
         <PhotoComp
           setChangeImage={setChangeImage}
           photo={data.response.results[0]}
         />
-      </>
+      </div>
     )
   }
 }
