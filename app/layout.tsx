78a56c5dd30e17dd89bd7a3fed06d9ad9d63ab00
Andrej Schreiner <schreiandrej@outlook.de>
2024-05-25 22:44:29 +0200

    chore: Update metadata for RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-'use client'
-
 // These styles apply to every route in the application
 import './globals.css'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'vorkasse-app',
+  description: 'Berechne die Vorkasse für deine Bestellung',
+}
 
 export default function RootLayout({
   children,
